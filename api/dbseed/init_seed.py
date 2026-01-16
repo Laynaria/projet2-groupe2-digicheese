8a90363d5ef9278dbf333e8a406869af4e2555b6
laynaria <pro.laynaria@outlook.fr>
2026-01-16 16:32:42 +0100

    fix du seeding de la DB qui ne se lançait pas, et correction pour éviter que le seed utilisateur se lance à chaque démarage du container

---
+++ api/dbseed/init_seed.py
@@ -0,0 +1,10 @@
+from .admin_user_seed import seed_admin_user
+from .objet_seed import seed_objets
+from .role_seed import seed_roles
+from .utilisateur_seed import seed_utilisateurs
+
+def init_seed():
+    seed_roles()
+    seed_admin_user()
+    seed_utilisateurs()
+    seed_objets()
