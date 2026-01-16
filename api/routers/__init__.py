16f1c240feaaf6810a35bd030de022bb164487b7
laynaria <pro.laynaria@outlook.fr>
2026-01-16 10:50:34 +0100

    router detail_commande terminé, importé dans l'init router et testé sur swagger.

--- api/routers/__init__.py
+++ api/routers/__init__.py
@@ -1,7 +1,9 @@
 from fastapi import APIRouter
 from .client_router import router as router_client
 from .commande_router import router as router_commande
+from .detail_commande_router import router as detail_commande_router
 
 router = APIRouter(prefix="/api/v1")
 router.include_router(router_client)
 router.include_router(router_commande)
+router.include_router(detail_commande_router)
