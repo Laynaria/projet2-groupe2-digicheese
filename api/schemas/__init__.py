fa7b4a1759d192e62a4619a32ed25f9cf2c826cb
laynaria <pro.laynaria@outlook.fr>
2026-01-16 12:12:33 +0100

    schema conditionnement terminé et importé dans le schema init

--- api/schemas/__init__.py
+++ api/schemas/__init__.py
@@ -1,5 +1,6 @@
 from .client_schema import ClientPost, ClientPatch, ClientInDB
 from .commande_schema import CommandePost, CommandePatch, CommandeInDB
+from .conditionnement_schema import ConditionnementPost, ConditionnementPatch, ConditionnementInDB
 from .detail_commande_schema import DetailCommandePost, DetailCommandePatch, DetailCommandeInDB
 from .utilisateur_schema import (
     UtilisateurCreate,
