94f414c6c06aefbee61e4b63a38f44f32f58b79b
laynaria <pro.laynaria@outlook.fr>
2026-01-16 14:10:56 +0100

    router conditionnement fonctionnel et importé dans le router init, correction de typo dans le router detail commande, reste à protéger et tester une dernière fois le router conditionnement

--- api/routers/__init__.py
+++ api/routers/__init__.py
@@ -3,11 +3,13 @@ from .auth_router import router as auth_router
 from .utilisateur_router import router as utilisateur_router
 from .client_router import router as router_client
 from .commande_router import router as router_commande
-from .detail_commande_router import router as detail_commande_router
+from .conditionnement_router import router as router_conditionnement
+from .detail_commande_router import router as router_detail_commande
 
 router = APIRouter(prefix="/api/v1")
 router.include_router(router_client)
 router.include_router(router_commande)
-router.include_router(detail_commande_router)
+router.include_router(router_conditionnement)
+router.include_router(router_detail_commande)
 router.include_router(auth_router)
 router.include_router(utilisateur_router)
---
+++ api/routers/conditionnement_router.py
@@ -0,0 +1,46 @@
+"""
+References all conditionnement-related endpoints in the FastAPI application.
+
+Receives requests from the conditionnement router, transform in conditionnement schema and process them using our ConditionnementService.
+"""
+
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.orm import Session
+from ..schemas import ConditionnementPost, ConditionnementPatch, ConditionnementInDB
+from ..database import get_db
+from ..services import ConditionnementService
+
+# Create a router for conditionnement-related endpoints
+router = APIRouter(prefix="/conditionnement", tags=["conditionnement"])
+
+# Initialize the conditionnement service to have acces to conditionnement operations
+service = ConditionnementService()
+
+@router.get("/", status_code=200, response_model=list[ConditionnementInDB])
+def get_conditionnements(db: Session=Depends(get_db)):
+    return service.get_all_conditionnements(db)
+
+@router.get("/{conditionnement_id}", status_code=200, response_model=ConditionnementInDB)
+def get_conditionnement(conditionnement_id: int, db: Session=Depends(get_db)):
+    conditionnement = service.get_conditionnement_by_id(db, conditionnement_id)
+    if conditionnement is None:
+        raise HTTPException(status_code=404, detail="Conditionnement non trouvé")
+    return conditionnement
+
+@router.post("/", status_code=201, response_model=ConditionnementInDB)
+def create_conditionnement(data_conditionnement: ConditionnementPost, db: Session=Depends(get_db)):
+    return service.create_conditionnement(db, data_conditionnement)
+
+@router.patch("/{conditionnement_id}", status_code=200, response_model=ConditionnementInDB)
+def patch_conditionnement(conditionnement_id: int, data_conditionnement: ConditionnementPatch, db: Session=Depends(get_db)):
+    conditionnement = service.get_conditionnement_by_id(db, conditionnement_id)
+    if conditionnement is None:
+        raise HTTPException(status_code=404, detail="Conditionnement non trouvé")
+    return service.patch_conditionnement(db, conditionnement_id, data_conditionnement)
+
+@router.delete("/{conditionnement_id}", status_code=200, response_model=ConditionnementInDB)
+def delete_conditionnement(conditionnement_id: int, db: Session=Depends(get_db)):
+    conditionnement = service.get_conditionnement_by_id(db, conditionnement_id)
+    if conditionnement is None:
+        raise HTTPException(status_code=404, detail="Conditionnement non trouvé")
+    return service.delete_conditionnement(db, conditionnement_id)
