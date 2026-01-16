e9f0d0d090af8e4b46b338d0952c2a62856d6a84
laynaria <pro.laynaria@outlook.fr>
2026-01-16 10:23:57 +0100

    schema detail_commande terminé, et importé dans le schéma init

--- api/schemas/__init__.py
+++ api/schemas/__init__.py
@@ -1,2 +1,3 @@
 from .client_schema import ClientPost, ClientPatch, ClientInDB
 from .commande_schema import CommandePost, CommandePatch, CommandeInDB
+from .detail_commande_schema import DetailCommandePost, DetailCommandePatch, DetailCommandeInDB
---
+++ api/schemas/detail_commande_schema.py
@@ -0,0 +1,30 @@
+from pydantic import BaseModel
+from datetime import date
+from typing import Optional
+
+class DetailCommandeBase(BaseModel):
+	"""Base schema for detail_commande data."""
+	quantite: int
+	colis: str
+	commentaire: str
+	commande_id: int
+	
+class DetailCommandePost(DetailCommandeBase):
+	"""Schema for creating new detail_commande."""
+	pass
+
+class DetailCommandePatch(DetailCommandeBase):
+	"""Schema for updating an existing detail_commande.
+	Values are all optionnal to update only what you want.
+	"""
+	quantite: Optional[int] = None
+	colis: Optional[str] = None
+	commentaire: Optional[str] = None
+	commande_id: Optional[int] = None
+
+class DetailCommandeInDB(DetailCommandeBase):
+	"""Schema for detail_commande data stored in Database"""
+	idDetailCommande: int
+
+	class Config:
+		from_attributes = True
