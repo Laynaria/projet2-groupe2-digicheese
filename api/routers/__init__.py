6d5c5a5427233e858002207005bc7aa088660aa1
Tracy Bachmann <tracy.bachmann68@gmail.com>
2026-01-16 13:32:28 +0100

    ajout de commune router et modification de l'init correspondant

--- api/routers/__init__.py
+++ api/routers/__init__.py
@@ -6,6 +6,7 @@ from .objet_router import router as router_objet
 from .commande_router import router as router_commande
 from .detail_commande_router import router as detail_commande_router
 from .role_router import router as role_router
+from .commune_router import router as commune_router
 
 router = APIRouter(prefix="/api/v1")
 router.include_router(router_client)
@@ -15,3 +16,4 @@ router.include_router(detail_commande_router)
 router.include_router(auth_router)
 router.include_router(utilisateur_router)
 router.include_router(role_router)
+router.include_router(commune_router)
