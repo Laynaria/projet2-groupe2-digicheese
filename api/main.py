51c7c1b5111565e6fdecdc87d72fd553ec226396
ThomasdevLC <thomas.lecam@gmail.com>
2026-02-03 13:57:08 +0100

    main config

--- api/main.py
+++ api/main.py
@@ -1,3 +1,4 @@
+import os
 from fastapi import FastAPI
 from .models import *
 from .database import engine
@@ -8,10 +9,21 @@ app = FastAPI()
 app.include_router(router)
 
 # Import the models to create the tables in the database
-Base.metadata.create_all(engine)
+# Base.metadata.create_all(engine)
+#
+# # Check if DB was already seed, and if not will seed with datas
+# init_seed()
 
-# Check if DB was already seed, and if not will seed with datas
-init_seed()
+
+# Create tables (souvent OK de le garder, mais je le rends aussi optionnel)
+CREATE_TABLES = os.getenv("CREATE_TABLES", "true").lower() == "true"
+SEED_DB = os.getenv("SEED_DB", "false").lower() == "true"
+
+if CREATE_TABLES:
+    Base.metadata.create_all(engine)
+
+if SEED_DB:
+    init_seed()
 
 # Define a simple root endpoint to check the connection
 @app.get("/")
